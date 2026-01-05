822e76981658091f0114024dc9b7cff2299942f4
Deep Makadia <deepmakadia1504@gmail.com>
2026-01-05 10:36:53 +0530

    - Implemented Add New Transaction

---
+++ app/src/main/java/com/dailyexpense/ui/states/NewTransactionState.kt
@@ -0,0 +1,22 @@
+package com.dailyexpense.ui.states
+
+import com.dailyexpense.data.enums.TransactionCategory
+import com.dailyexpense.data.enums.TransactionType
+import com.dailyexpense.data.models.TransactionTypeChipData
+import com.dailyexpense.data.room.entity.AccountEntity
+import com.dailyexpense.data.room.entity.CategoryEntity
+import com.dailyexpense.data.room.entity.TagEntity
+
+data class NewTransactionState(
+    val transactionType: TransactionType,
+    val amount: String,
+    val selectedCategory: CategoryEntity?,
+    val selectedAccount: AccountEntity?,
+    val selectedPaymentMethod: TransactionCategory?,
+    val notes: String,
+    val selectedDate: Long,
+    val transactionTypeChipData: List<TransactionTypeChipData>,
+    val selectedTags: List<TagEntity>,
+    val tagSearchQuery: String,
+    val suggestedTags: List<TagEntity>
+)
